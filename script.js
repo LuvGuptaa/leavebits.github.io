9efcdd869775f293616a4ee4a69aca465e9d52ab
Shwetabh Niket <niketshwetabh@gmail.com>
2023-10-15 12:12:34 +0530

    leave gen for shankar

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const { degrees, PDFDocument, rgb, StandardFonts } = PDFLib
 
-    async function modifyPdf() {
+async function modifyPdf() {
     // Fetch an existing PDF document
     const url = 'leave.pdf'
-        const existingPdfBytes = await fetch(url).then(res => res.arrayBuffer())
+    const existingPdfBytes = await fetch(url).then(res => res.arrayBuffer())
 
     // Load a PDFDocument from the existing PDF bytes
     const pdfDoc = await PDFDocument.load(existingPdfBytes)
@@ -16,7 +16,7 @@ const { degrees, PDFDocument, rgb, StandardFonts } = PDFLib
     const firstPage = pages[0]
 
     var ID = document.getElementById("ID");
-    var name = document.getElementById("name");
+    var name = document.getElementById("name")
     var contact = document.getElementById("contact");
     var room = document.getElementById("room");
     var hostel = document.getElementById("hostel");
@@ -25,69 +25,69 @@ const { degrees, PDFDocument, rgb, StandardFonts } = PDFLib
     var returnndate = new Date(returnn);
     var departuredate = new Date(departure);
     var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-    var wardens = ['Rajesh Kumar', 'Srinivas Appari', 'Nitin Chaturvedi', 'Krishnendra Shekhawat', 'Surekha Bhanot', 'Kumar Sankar Bhattacharya', 'Praveen Kumar A.V.', 'MM Pandey', 'Sharad Shrivastava']
-    var hostels = ['Srinivasa Ramanujan Bhawan', 'Krishna Bhawan', 'Gandhi Bhawan', 'Vishwakarma Bhawan', 'Meera Bhawan', 'Shankar Bhawan', 'Vyas Bhawan', 'Ram Bhawan', 'Budh Bhawan']
+    var wardens = ['Sharad Shrivastava', 'Srinivas Appari', 'Srinivas Appari', 'Nitin Chaturvedi', 'Krishnendra Shekhawat', 'Surekha Bhanot', 'Kumar Sankar Bhattacharya', 'Praveen Kumar A.V.', 'MM Pandey']
+    var hostels = ['Shankar Bhawan', 'Krishna Bhawan', 'Krishna Bhawan', 'Gandhi Bhawan', 'Vishwakarma Bhawan', 'Meera Bhawan', 'Vyas Bhawan', 'Ram Bhawan', 'Budh Bhawan']
 
     // Get the width and height of thne first page
     const { width, height } = firstPage.getSize()
     console.log(width, height)
     firstPage.drawText(ID.value, {
-        x: 302,  
-        y: 710, 
+        x: 302,
+        y: 710,
         size: 12.2,
         font: helveticaFont,
         color: rgb(0, 0, 0)
     })
-    firstPage.drawText(name.value, {
-        x: 302,  
-        y: 688,  
+    firstPage.drawText(name.value.toUpperCase(), {
+        x: 302,
+        y: 688,
         size: 12.2,
         font: helveticaFont,
         color: rgb(0, 0, 0)
     })
     firstPage.drawText(contact.value, {
-        x: 302,  
-        y: 667,  
+        x: 302,
+        y: 667,
         size: 12.2,
         font: helveticaFont,
         color: rgb(0, 0, 0)
     })
     firstPage.drawText(hostel.value, {
-        x: 302,  
-        y: 647,  
+        x: 302,
+        y: 647,
         size: 12.2,
         font: helveticaFont,
         color: rgb(0, 0, 0)
     })
     firstPage.drawText(room.value, {
-        x: 302,  
-        y: 627,  
+        x: 302,
+        y: 627,
         size: 12.2,
         font: helveticaFont,
         color: rgb(0, 0, 0)
     })
     firstPage.drawText(wardens[hostels.indexOf(hostel.value)], {
-        x: 302,  
-        y: 607,  
+        x: 302,
+        y: 607,
         size: 12.2,
         font: helveticaFont,
         color: rgb(0, 0, 0)
     })
-    firstPage.drawText(departuredate.getDate().toString()+'-'+months[departuredate.getMonth()]+'-'+(departuredate.getYear()-100+2000).toString(), {
-        x: 302,  
-        y: 587, 
+    firstPage.drawText(departuredate.getDate().toString() + '-' + months[departuredate.getMonth()] + '-' + (departuredate.getYear() - 100 + 2000).toString(), {
+        x: 302,
+        y: 587,
         size: 12.2,
         font: helveticaFont,
         color: rgb(0, 0, 0)
     })
-    firstPage.drawText(returnndate.getDate().toString()+'-'+months[returnndate.getMonth()]+'-'+(returnndate.getYear()-100+2000).toString(), {
-        x: 302,  
-        y: 567,  
+    firstPage.drawText(returnndate.getDate().toString() + '-' + months[returnndate.getMonth()] + '-' + (returnndate.getYear() - 100 + 2000).toString(), {
+        x: 302,
+        y: 567,
         size: 12.2,
         font: helveticaFont,
         color: rgb(0, 0, 0)
     })
     const pdfBytes = await pdfDoc.save()
-    download(pdfBytes, ID.value+'.pdf', 'application/pdf');
-    }
+    download(pdfBytes, ID.value + '.pdf', 'application/pdf');
+}
 
